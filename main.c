b3b038c6413c614bc9a941408992a013c42556d8
MZainAnsari <153723931+MZainAnsari@users.noreply.github.com>
2023-12-13 15:00:15 +0500

    First file uploaded for testing

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+  char *days[7];
+  char TheDay[10];
+  int day;
+
+  days[0] = "Sunday";
+  days[1] = "Monday";
+  days[2] = "Tuesday";
+  days[3] = "Wednesday";
+  days[4] = "Thursday";
+  days[5] = "Friday";
+  days[6] = "Saturday";
+  
